bbfe6f744fb4265d3db7268f48fdf556bb51fffd
unknown <kunal.chaudhary@act21.io>
2025-02-12 12:39:36 +0530

    BasicProjectWithBabelConfig

---
+++ babel.config.js
@@ -0,0 +1,20 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: [
+      "babel-preset-expo",
+     
+      [
+        "@babel/preset-react",
+        { 
+          runtime: "automatic",
+          importSource: '@emotion/react' 
+        } 
+      ],
+     
+    ],
+    plugins: [
+      ["inline-react-svg"]
+    ],
+  };
+};

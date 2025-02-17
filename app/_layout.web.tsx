e88883bd9f95924bb83fdf12d7885226e9d46532
unknown <kunal.chaudhary@act21.io>
2025-02-17 12:57:05 +0530

    RenameIndex.ts

--- app/_layout.web.tsx
+++ app/_layout.web.tsx
@@ -6,7 +6,7 @@ export default function WebLayout() {
   const router = useRouter();
   useEffect(() => {
     if (window.location.pathname === "/") {
-      router.replace("/(web)");
+      router.replace("/(web)/Login");
     }  
   }, []);
   return <Slot />;

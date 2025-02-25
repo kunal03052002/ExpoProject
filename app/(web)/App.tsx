85afba7d0e86dcecac94affdf71cb3396e42f994
unknown <kunal.chaudhary@act21.io>
2025-02-25 19:15:02 +0530

    newchanges

--- app/(web)/App.tsx
+++ app/(web)/App.tsx
@@ -1,55 +1,13 @@
-// import { MutableRefObject, useRef } from "react";
-import { DndProvider } from "react-dnd";
-import Editor from "../../page/app";
-import { ConfigProvider, enUS } from "../../utils/configProvider";
-// import { useTranslation } from "react-i18next";
-import { useEffect } from "react";
-// import { globalStyle } from "./style";
-import { Global } from "@emotion/react";
-import { ModalGroup } from "../../utils/model/src/modal-group";
-// import { TouchBackend } from "react-dnd-touch-backend";
-import { NotificationGroup } from "../../utils/Notification";
-// import { useParams } from "react-router-dom";
-import axios from "axios";
 import { Provider } from "react-redux";
 import store from "@/store";
-import { HTML5Backend } from "react-dnd-html5-backend";
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-function App(props:any) {
-//   const { id } = useParams();
-  const currentUserLanguage = "en-US";
-  const dragOptions = {
-    enableTouchEvents: true,
-    enableMouseEvents: true,
-  };
-//   const { i18n } = useTranslation();
-
-//   useEffect(() => {
-//     if (currentUserLanguage) {
-//       i18n.changeLanguage(currentUserLanguage);
-//     }
-//     // axios.get(`${"http://52.183.132.161/backend"}/api/apps/${id}`).then((res) => {
-//     //   localStorage.setItem("pageConfig", JSON.stringify(res.data.config));
-//     // });
-//   }, [currentUserLanguage, i18n, id]);
-  return ( 
+function App(props: any) {
+  return (
     <>
-    <DndProvider backend={HTML5Backend}>
-    <Provider store={store}>
-      <div style={{ boxSizing: "border-box", overflow: "hidden" }}>
-        {/* <DndProvider backend={TouchBackend} options={dragOptions}> */}
-          <ConfigProvider locale={enUS}>
-            {/* <Global styles={globalStyle} /> */}
-            {/* <NotificationGroup pt={"46px"} /> */}
-            {/* <ModalGroup /> */}
-            {/* <Editor {...props}/> */}
-          </ConfigProvider>
-        {/* </DndProvider> */}
-      </div>
+      <Provider store={store}>
+        <div style={{ boxSizing: "border-box", overflow: "hidden" }}>
+          <div>Hello World</div>
+        </div>
       </Provider>
-      </DndProvider>
-      
     </>
   );
 }
--- widgetLibrary/ContainerWidget/index.tsx
+++ widgetLibrary/ContainerWidget/index.tsx
@@ -1,4 +1,3 @@
+// impaktapps-ui-builder\widgetLibrary\ContainerWidget\index.tsx
 export { CONTAINER_WIDGET_CONFIG } from "./widgetConfig"
-export { CONTAINER_PANEL_CONFIG } from "./panelConfig"
-export {ContainerWidget} from './container'
-// export {CONTAINER_WIDGET_CONFIG3 as CONTAINER_WIDGET_CONFIG2} from "./widgetBuilder2"
+
